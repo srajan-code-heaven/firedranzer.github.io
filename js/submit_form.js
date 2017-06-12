a8b76b9dfb86797d5b8d6d0a88895379fc3b23db
rpz-harode <rupeshharode@gmail.com>
2017-06-12 17:08:12 +0530

    Linked request form with database

---
+++ js/submit_form.js
@@ -0,0 +1,10 @@
+// Submit form with id function.
+function submit_by_id() {
+var name = document.getElementById("name").value;
+var email = document.getElementById("email").value;
+if (validation()) // Calling validation function
+{
+document.getElementById("form_id").submit(); //form submission
+alert(" Name : " + name + " \n Email : " + email + " \n Form Id : " + document.getElementById("form_id").getAttribute("id") + "\n\n Form Submitted Successfully......");
+}
+}
